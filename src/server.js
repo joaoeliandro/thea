dea4189e6ec6685ebbfe503b17976b710b003329
João Eliandro <eliandrogermano@gmail.com>
2020-09-20 20:22:51 -0300

    Adicionado o cors e paramentro de url no receive

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const zenvia = require("./services/zenvia");
 
 const app = express();
+
+app.use(cors());
+
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" });
@@ -19,4 +24,4 @@ app.listen(3333, () => {
   console.log("Server started!");
 });
 
-zenvia.receiveMessage();
+zenvia.receiveMessage("https://webhook.site/4a5589fc-7776-4a4e-9ce7-75b6f953f538");
