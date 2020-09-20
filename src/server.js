e49a13ed25155a5b19fa61ecde531f565a5d3fac
João Eliandro <eliandrogermano@gmail.com>
2020-09-20 01:18:07 -0300

    Adicionado rota de envio de mensagem

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const zenvia = require("./services/zenvia");
 
 const app = express();
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" });
+});
+
+app.get("/send", async (request, response) => {
+  const { message } = request.query;
+
+  const result = await zenvia.sendMessage(message);
+
+  console.log(result);
 });
 
 app.listen(3333, () => {
