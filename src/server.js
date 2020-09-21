8d854f9c7f6662aa5060da639c5f1eb060f64a1e
João Eliandro <eliandrogermano@gmail.com>
2020-09-20 22:31:45 -0300

    Adicionado boduparser e verificação de mensagem

--- src/server.js
+++ src/server.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const zenvia = require("./services/zenvia");
 
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" });
 });
 
-app.get("/send", async (request, response) => {
-  const { message } = request.query;
+app.post("/send-message", async (request, response) => {
+  const { message } = request.body;
 
-  const result = await zenvia.sendMessage(message);
+  if (message === "init") {
+    zenvia.receiveMessage("https://webhook.site/4a5589fc-7776-4a4e-9ce7-75b6f953f538");
 
-  return response.json(result);
+    return response.send("ok");
+  }
+
+  else {
+    const result = await zenvia.sendMessage(message);
+
+    return response.json(result);
+  }
 });
 
 app.listen(3333, () => {
   console.log("Server started!");
 });
 
-zenvia.receiveMessage("https://webhook.site/4a5589fc-7776-4a4e-9ce7-75b6f953f538");
